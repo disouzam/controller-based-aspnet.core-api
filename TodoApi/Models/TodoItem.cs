cc586faaf7d762a732955668fffc3717685dfa5b
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-07-04 17:32:27 -0300

    Adjusted current classes to use file-scoped namespaces

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -1,11 +1,10 @@
-﻿namespace TodoApi.Models
+﻿namespace TodoApi.Models;
+
+public class TodoItem
 {
-    public class TodoItem
-    {
-        public long Id { get; set; }
+    public long Id { get; set; }
 
-        public string? Name { get; set; }
+    public string? Name { get; set; }
 
-        public bool IsComplete { get; set; }
-    }
+    public bool IsComplete { get; set; }
 }
