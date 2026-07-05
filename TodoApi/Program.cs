9b96b07513b7dbb0d6e0e24efb43175b1d0938d2
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-07-05 14:34:03 -0300

    Temporary retrieval of token in Program.cs
    - Later this retrieval will happen in the controller SecretExpirationDateController to proof the feasibility of safely get expiration date

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using TodoApi.MicrosoftEntraId;
+using TodoApi.MicrosoftGraph;
 using TodoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +21,9 @@ if (connectionString is null)
 
 var settings = Settings.LoadSettings();
 
+// Initialize Graph
+InitializeGraph(settings);
+
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
@@ -29,6 +34,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Display access token
+await DisplayAccessTokenAsync();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -44,3 +52,22 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+void InitializeGraph(Settings settings)
+{
+    GraphHelper.InitializeGraphForAppOnlyAuth(settings);
+}
+
+async Task DisplayAccessTokenAsync()
+{
+    try
+    {
+        var appOnlyToken = await GraphHelper.GetAppOnlyTokenAsync();
+        Console.WriteLine($"App-only token: {appOnlyToken}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting app-only access token: {ex.Message}");
+    }
+}
