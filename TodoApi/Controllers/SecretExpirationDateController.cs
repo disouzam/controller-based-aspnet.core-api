5948739bbb0881bcda26d062badd5c8c336e4b2b
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-07-05 14:44:28 -0300

    Dummy implementation of GET endpoint in a new controller `SecretExpirationDateController`

---
+++ TodoApi/Controllers/SecretExpirationDateController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using TodoApi.Dtos;
+
+namespace TodoApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SecretExpirationDateController : ControllerBase
+{
+    // GET: api/SecretExpirationDate/5
+    [HttpGet("{secretId}")]
+    public async Task<ActionResult<SecretExpirationDateDto>> GetSecretExpirationDate(Guid secretId)
+    {
+        var result = new SecretExpirationDateDto
+        {
+            SecretId = secretId,
+            ExpirationDate = DateTime.UtcNow.AddDays(30) // Example expiration date
+        };
+
+        return Ok(result);
+    }
+}
